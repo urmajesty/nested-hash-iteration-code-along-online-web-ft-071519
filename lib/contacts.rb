59e98b85f5355c91a4e1f791a5e50edc158ca53d
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-29 18:50:32 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,6 +19,8 @@ def remove_strawberry(contacts)
         if person == "Freddy Mercury"
           contact_details_hash.each do |attribute, data|
             if attribute == :favorite_ice_cream_flavors
+              data.delete_if {|ice_cream|
+              ice_cream == "strawberry"
             binding.pry
    end
  end

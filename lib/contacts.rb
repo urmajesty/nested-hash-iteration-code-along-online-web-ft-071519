ba96ef8bee47cd6a2d8b6e8d61b27d762ca0404a
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-29 18:45:02 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,8 @@
   
 def remove_strawberry(contacts)
       contacts.each do |person, contact_details_hash|
-      
+        if person == "Freddy Mercury"
+          contact_details_hash.each do |attribute, data|
             binding.pry
    end
       

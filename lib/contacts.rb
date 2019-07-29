4b61868b6e1cc5214e7b3acf776a6665f5ee10ee
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-29 18:42:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,9 +18,7 @@ def remove_strawberry(contacts)
       contacts.each do |person, contact_details_hash|
       
             binding.pry
-          end
-        
-      end
+   end
       
    end
         
